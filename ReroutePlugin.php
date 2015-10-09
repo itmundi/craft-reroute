e6bd9af97f28b7e6dadc8a00c1fc395c6d92a255
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2015-10-09 10:52:40 +0200

    Fixed use with console

--- ReroutePlugin.php
+++ ReroutePlugin.php
@@ -4,47 +4,48 @@ namespace Craft;
 
 class ReroutePlugin extends BasePlugin
 {
-	public function getName()
-	{
-		return Craft::t('Reroute');
-	}
+    public function getName()
+    {
+        return Craft::t('Reroute');
+    }
 
-	public function getVersion()
-	{
-		return '1.0.3';
-	}
+    public function getVersion()
+    {
+        return '1.0.3';
+    }
 
-	public function getDeveloper()
-	{
-		return 'Trevor Davis';
-	}
+    public function getDeveloper()
+    {
+        return 'Trevor Davis';
+    }
 
-	public function getDeveloperUrl()
-	{
-		return 'http://trevordavis.net';
-	}
+    public function getDeveloperUrl()
+    {
+        return 'http://trevordavis.net';
+    }
 
-	public function hasCpSection()
-	{
-		return true;
-	}
+    public function hasCpSection()
+    {
+        return true;
+    }
 
-	public function registerCpRoutes()
-	{
-		return array(
-			'reroute\/new' => 'reroute/_form',
-			'reroute\/(?P<rerouteId>\d+)' => 'reroute/_form',
-		);
-	}
+    public function registerCpRoutes()
+    {
+        return array(
+            'reroute\/new' => 'reroute/_form',
+            'reroute\/(?P<rerouteId>\d+)' => 'reroute/_form',
+        );
+    }
 
-	public function init() {
-		if(craft()->request->isSiteRequest()) {
-			$url = craft()->request->getUrl();
-			$reroute = craft()->reroute->getByUrl($url);
+    public function init()
+    {
+        if (!craft()->isConsole() && craft()->request->isSiteRequest()) {
+            $url = craft()->request->getUrl();
+            $reroute = craft()->reroute->getByUrl($url);
 
-			if ($reroute) {
-				craft()->request->redirect($reroute['newUrl'], true, $reroute['method']);
-			}
-		}
-	}
+            if ($reroute) {
+                craft()->request->redirect($reroute['newUrl'], true, $reroute['method']);
+            }
+        }
+    }
 }
